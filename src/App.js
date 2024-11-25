c0872e7ff0c31c38fcae56d29f5d1b80e01ce43a
PC2408 <applelgy@naver.com>
2024-11-25 20:09:39 +0900

    최초커밋

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1> 클론코딩 가보자고</h1>
         </p>
         <a
           className="App-link"
